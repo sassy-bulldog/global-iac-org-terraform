bd15047cb8c3d4219b44dd20b4c1778640937899
David White <24359398+actuarysailor@users.noreply.github.com>
2024-09-19 07:59:15 -0400

    feat: Initial deployment of data lake

---
+++ enterprise/datalake.tf
@@ -0,0 +1,22 @@
+resource "azurerm_storage_account" "lake" {
+  count = var.create.data_lake ? 1 : 0
+
+  name                     = "${var.resource_group}-data-lake-storage"
+  resource_group_name      = azurerm_resource_group.this.name
+  location                 = azurerm_resource_group.this.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  account_kind             = "StorageV2"
+  is_hns_enabled           = "true"
+  tags                     = local.default_tags
+}
+
+resource "azurerm_storage_data_lake_gen2_filesystem" "data_lake" {
+  name               = "${var.resource_group}-data-lake"
+  storage_account_id = azurerm_storage_account.lake.id
+  # default_encryption_scope 
+
+#   properties = {
+#     hello = "aGVsbG8="
+#   }
+}

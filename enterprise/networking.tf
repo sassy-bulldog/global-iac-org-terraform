bba24d59b7caf195805db52bebca0a2a2a323dc4
David White <24359398+actuarysailor@users.noreply.github.com>
2024-09-18 21:07:17 -0400

    fix: Resource group namespace

--- enterprise/networking.tf
+++ enterprise/networking.tf
@@ -1,7 +1,7 @@
 resource "azurerm_virtual_network" "company" {
   name                = "${var.resource_group}-vnet"
-  resource_group_name = azurerm_resource_group.company.name
-  location            = azurerm_resource_group.company.location
+  resource_group_name = azurerm_resource_group.this.name
+  location            = azurerm_resource_group.this.location
   address_space       = [var.azure_vnet_cidr_block]
 
   tags = local.default_tags
